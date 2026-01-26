36c6c82dbc80a57b1aefc3ae41a7409dee61845a
RigelNostdlib <edo.nostdlib@gmail.com>
2026-01-26 17:25:17 +0400

    .

--- src/ral/crypt/random/windows/random.windows.cc
+++ src/ral/crypt/random/windows/random.windows.cc
@@ -1,36 +1,56 @@
 #include "random.h"
 #include "date_time.h"
-#include "pal.h"
+#include <stdint.h>
+
+// Helper to mix entropy bits (prevents similar seeds from producing similar sequences)
+static uint32_t MixBits(uint32_t x)
+{
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return x;
+}
 
 INT32 Random::GetSeedFromTime()
 {
     auto dateTime = DateTime::Now();
 
-    // Make a 64-bit value combining all time components
+    // Combine ms, us, and seconds into a 64-bit block
     UINT64 t = 0;
-    t |= (UINT64)dateTime.Milliseconds << 32; // cast first!
-    t |= (UINT64)dateTime.Microseconds << 12; // shift enough to avoid overlap
-    t |= (UINT64)dateTime.Second;             // seconds in lowest bits
+    t |= (UINT64)dateTime.Milliseconds << 32;
+    t |= (UINT64)dateTime.Microseconds << 12;
+    t |= (UINT64)dateTime.Second;
 
-    // Mix down to 32-bit seed
+    // Fold 64-bit time into 32-bit using XOR
     return (UINT32)(t ^ (t >> 32));
 }
 
-// Function to get a random number in the range of 0 to RANDOM_MAX
-INT32 Random::Get()
-{
-    UINT32 dummmyVar = 0;
-    this->seed = (this->seed * 214013L + 2531011L + (INT32)(SSIZE)(PVOID)&dummmyVar) & 0xffffffff;
-    // Generate a new random number using a linear congruential generator formula
-    return (INT32)(this->seed % Random::MAX);
-}
-
-// Constructor to initialize the random number generator
 Random::Random()
 {
-    // Seed using high-resolution time + this pointer + stack variable
-    SSIZE ptr = (SSIZE)this;
-    SSIZE stack_var = (SSIZE)&ptr;
-    SSIZE seedFromTime = GetSeedFromTime();
-    this->seed = (UINT32)((seedFromTime ^ ptr ^ stack_var ^ (SSIZE)&seedFromTime) & 0xFFFFFFFF);
+    // Collect various sources of "noise"
+    UINT32 timeSeed = (UINT32)GetSeedFromTime();
+    USIZE ptrSeed = (USIZE)this;
+
+    // Mix them together
+    this->seed = MixBits(timeSeed ^ (UINT32)ptrSeed);
+
+    // Xorshift seed must never be 0
+    if (this->seed == 0)
+    {
+        this->seed = 0xACE1u;
+    }
+}
+
+INT32 Random::Get()
+{
+    // Xorshift32 algorithm: Fast and passes most statistical tests
+    UINT32 x = this->seed;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    this->seed = x;
+
+    // Use 64-bit math to map the result to [0, Random::MAX)
+    // This significantly reduces "Modulo Bias"
+    return (INT32)(((UINT64)x * (UINT64)Random::MAX) >> 32);
 }

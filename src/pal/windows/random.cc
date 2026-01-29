4055f9e2c0e9e97306c14b45a1b9359df6fd46b4
RigelNostdlib <edo.nostdlib@gmail.com>
2026-01-29 14:36:54 +0400

    one more time

--- src/pal/windows/random.cc
+++ src/pal/windows/random.cc
@@ -5,15 +5,31 @@
 #include "peb.h"
 #include "logger.h"
 
-extern "C"
+static inline unsigned long long GetHardwareTimestamp()
 {
-    unsigned long long __rdtsc(void);
+#if defined(__x86_64__) || defined(_M_X64) || defined(__i386__) || defined(_M_IX86)
+    // x86/x64: Read the Time Stamp Counter
+    unsigned int lo, hi;
+    __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+    return ((unsigned long long)hi << 32) | lo;
+
+#elif defined(__aarch64__) || defined(_M_ARM64)
+    // ARM64: Read the Virtual Counter Register (CNTVCT_EL0)
+    unsigned long long virtual_timer_value;
+    __asm__ __volatile__("mrs %0, cntvct_el0" : "=r"(virtual_timer_value));
+    return virtual_timer_value;
+
+#else
+    // Fallback for other architectures (less precise)
+    return 0;
+#endif
 }
 
 UINT64 Random::GetSeedFromTime()
 {
+
     // Mix down to 32-bit seed
-    return (UINT64)(__rdtsc());
+    return (UINT64)(GetHardwareTimestamp());
 }
 
 // Function to get a random number in the range of 0 to RANDOM_MAX

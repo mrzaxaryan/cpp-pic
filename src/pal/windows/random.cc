24b7740c2c6e3d3c3dcc9d8c5f56b5c2c84c26e4
RigelNostdlib <edo.nostdlib@gmail.com>
2026-01-29 14:33:01 +0400

    test

--- src/pal/windows/random.cc
+++ src/pal/windows/random.cc
@@ -5,20 +5,22 @@
 #include "peb.h"
 #include "logger.h"
 
+extern "C"
+{
+    unsigned long long __rdtsc(void);
+}
+
 UINT64 Random::GetSeedFromTime()
 {
-    // Get current date and time
-    auto dateTime = DateTime::Now();
-
     // Mix down to 32-bit seed
-    return (UINT64)(dateTime.Microseconds * dateTime.Microseconds);
+    return (UINT64)(__rdtsc());
 }
 
 // Function to get a random number in the range of 0 to RANDOM_MAX
 INT32 Random::Get()
 {
     // simple linear congruential generator
-    seed = (seed * (UINT64)214013 + (UINT64)426346) & 0x7FFFFFFF;
+    seed = (seed * GetSeedFromTime() + (UINT64)214013) & 0x7FFFFFFF;
     Logger::Debug<WCHAR>(L"[Random] Generated value: %u"_embed, static_cast<UINT32>((seed >> 16) & 0x7FFF));
     return static_cast<INT32>(seed % MAX);
 }

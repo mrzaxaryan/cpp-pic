3c988727e4554f14b1943f8429eac1077b5d2688
mrzaxaryan <mrzaxaryan>
2026-02-04 11:41:09 -0800

    Add entry point implementation for CPP-PIC runtime across platforms

---
+++ src/entry_point.cc
@@ -0,0 +1,34 @@
+/**
+ * start.cc - CPP-PIC Runtime Entry Point
+ *
+ * Unified entry_point() entry point for all platforms.
+ */
+
+#include "runtime.h"
+
+INT32 start();
+
+/**
+ * entry_point - Entry point for all platforms
+ */
+#if defined(PLATFORM_UEFI)
+ENTRYPOINT EFI_STATUS EFIAPI entry_point(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+#else
+ENTRYPOINT INT32 entry_point(VOID)
+#endif
+{
+#if defined(PLATFORM_UEFI)
+	// Allocate context on stack and store pointer in CPU register (GS/TPIDR_EL0)
+	// This eliminates the need for a global variable in .data section
+	EFI_CONTEXT efiContext = {};
+	efiContext.ImageHandle = ImageHandle;
+	efiContext.SystemTable = SystemTable;
+	SetEfiContextRegister(&efiContext);
+	// Disable watchdog timer (default is 5 minutes)
+	SystemTable->BootServices->SetWatchdogTimer(0, 0, 0, NULL);
+#endif
+
+	// Run runtime and unit tests
+	BOOL allPassed = start();
+	ExitProcess(allPassed ? 0 : 1);
+}

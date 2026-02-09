3b45909b34748356dd51e697c6c6489f48c1ac4d
CelesteNostdlib <rnostdlib@gmail.com>
2026-02-09 01:38:13 -0800

    Refactor HttpClient class documentation for clarity and consistency

--- include/network/http.h
+++ include/network/http.h
@@ -3,6 +3,7 @@
 #include "platform.h"
 #include "tls.h"
 
+// HttpClient class for making HTTP requests, supporting both secure (HTTPS) and non-secure (HTTP) connections
 class HttpClient
 {
 private:
@@ -15,9 +16,10 @@ private:
     Socket socketContext;
 
 public:
+    // Constructors for HttpClient class, allowing initialization with a URL and optional IP address
     HttpClient(PCCHAR url, PCCHAR ipAddress);
     HttpClient(PCCHAR url);
-
+    // Operations with HttpClient
     BOOL Open();
     BOOL Close();
     SSIZE Read(PVOID buffer, UINT32 bufferLength);
@@ -25,8 +27,8 @@ public:
 
     BOOL SendGetRequest();
     BOOL SendPostRequest(PCVOID data, UINT32 dataLength);
-
+    // Destructor to clean up resources
     ~HttpClient();
-
+    // Static method to parse a URL into its components (host, path, port, secure) and validate the format
     static BOOL ParseUrl(PCCHAR url, PCHAR host, PCHAR path, PUINT16 port, PBOOL secure);
 };

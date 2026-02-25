c8c3eeb9da6b5d258125aa097e25a641e30e3bce
mrzaxaryan <mrzaxaryan>
2026-02-25 02:02:42 -0800

    Implement wide string writing for console across platforms with UTF-16 to UTF-8 conversion

--- src/platform/linux/console.cc
+++ src/platform/linux/console.cc
@@ -1,10 +1,40 @@
 #include "console.h"
 #include "syscall.h"
 #include "system.h"
+#include "encoding/utf16.h"
 
 // Write ANSI/ASCII string to console (straightforward)
 UINT32 Console::Write(const CHAR *text, USIZE length)
 {
     SSIZE result = System::Call(SYS_WRITE, STDOUT_FILENO, (USIZE)text, length);
     return (result >= 0) ? (UINT32)result : 0;
+}
+
+// Write wide string to console (WCHAR is 4-byte UTF-32 on Linux)
+UINT32 Console::Write(const WCHAR *text, USIZE length)
+{
+    constexpr USIZE BUFFER_SIZE = 256;
+    CHAR utf8[BUFFER_SIZE];
+    UINT32 totalWritten = 0;
+    USIZE bufIndex = 0;
+
+    for (USIZE i = 0; i < length; i++)
+    {
+        CHAR bytes[4];
+        USIZE n = UTF16::CodepointToUTF8Bytes((UINT32)text[i], bytes);
+
+        if (bufIndex + n > BUFFER_SIZE)
+        {
+            totalWritten += Write(utf8, bufIndex);
+            bufIndex = 0;
+        }
+
+        for (USIZE j = 0; j < n; j++)
+            utf8[bufIndex++] = bytes[j];
+    }
+
+    if (bufIndex > 0)
+        totalWritten += Write(utf8, bufIndex);
+
+    return totalWritten;
 }
--- src/platform/windows/console.cc
+++ src/platform/windows/console.cc
@@ -2,6 +2,7 @@
 #include "platform.h"
 #include "ntdll.h"
 #include "peb.h"
+#include "encoding/utf16.h"
 
 UINT32 Console::Write(const CHAR *text, USIZE length)
 {
@@ -10,4 +11,34 @@ UINT32 Console::Write(const CHAR *text, USIZE length)
 	Memory::Zero(&ioStatusBlock, sizeof(IO_STATUS_BLOCK));
 	NTDLL::ZwWriteFile(peb->ProcessParameters->StandardOutput, NULL, NULL, NULL, &ioStatusBlock, (PVOID)text, length, NULL, NULL);
 	return (UINT32)ioStatusBlock.Information;
+}
+
+UINT32 Console::Write(const WCHAR *text, USIZE length)
+{
+	constexpr USIZE BUFFER_SIZE = 256;
+	CHAR utf8[BUFFER_SIZE];
+	UINT32 totalWritten = 0;
+
+	USIZE inputIndex = 0;
+	USIZE bufIndex = 0;
+
+	while (inputIndex < length)
+	{
+		CHAR bytes[4];
+		USIZE n = UTF16::CodepointToUTF8(text, length, inputIndex, bytes);
+
+		if (bufIndex + n > BUFFER_SIZE)
+		{
+			totalWritten += Write(utf8, bufIndex);
+			bufIndex = 0;
+		}
+
+		for (USIZE j = 0; j < n; j++)
+			utf8[bufIndex++] = bytes[j];
+	}
+
+	if (bufIndex > 0)
+		totalWritten += Write(utf8, bufIndex);
+
+	return totalWritten;
 }

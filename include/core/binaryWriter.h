940055aee99626201de8cb287d3de162fc9514e2
CelesteNostdlib <rnostdlib@gmail.com>
2026-02-26 04:28:11 -0800

    ref: update BinaryWriter methods

--- include/core/binaryWriter.h
+++ include/core/binaryWriter.h
@@ -9,23 +9,23 @@ class BinaryWriter{
     public:
         BinaryWriter(PVOID address, USIZE offset, USIZE maxSize) : address(address), offset(offset), maxSize(maxSize) {}
         template<typename T>
-        static PVOID Write(BinaryWriter* writer, T value){
-            if(writer->offset + sizeof(T) > writer->maxSize){
+        PVOID Write(T value){
+            if(offset + sizeof(T) > maxSize){
                 return nullptr;
             }
 
-            *(T*)((PCHAR)writer->address + writer->offset) = value;
-            writer->offset += sizeof(T);
-            return writer->address;
+            *(T*)((PCHAR)address + offset) = value;
+            offset += sizeof(T);
+            return address;
         }
 
-        static PVOID WriteBytes(BinaryWriter* writer, PCHAR data, USIZE size){
-            if(writer->offset + size > writer->maxSize){
+        PVOID WriteBytes(PCHAR data, USIZE size){
+            if(offset + size > maxSize){
                 return nullptr;
             }
 
-            Memory::Copy((PCHAR)writer->address + writer->offset, data, size);
-            writer->offset += size;
-            return writer->address;
+            Memory::Copy((PCHAR)address + offset, data, size);
+            offset += size;
+            return address;
         }
 };
